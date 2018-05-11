972aab52901f41b3df3db0b35df2c090ca752810
Bogdan <Bogdan@LAPTOP-CHTJCNUO>
2018-05-11 21:55:56 +0300

    start projct

---
+++ src/mathdiffer/controller/MainWinController.java
@@ -0,0 +1,14 @@
+package mathdiffer.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class MainWinController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
