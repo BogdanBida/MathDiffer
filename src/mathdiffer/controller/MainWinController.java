02b383ebcc9ed77cd5f1604fa82f2c756fc48bd3
Bogdan <Bogdan@LAPTOP-CHTJCNUO>
2018-05-21 22:42:37 +0300

    -

--- src/mathdiffer/controller/MainWinController.java
+++ src/mathdiffer/controller/MainWinController.java
@@ -2,14 +2,30 @@ package mathdiffer.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import mathdiffer.Calculation;
 
 public class MainWinController implements Initializable {
 
+    @FXML
+    private TextField field_a;
+
+    @FXML
+    private TextField field_b;
+
+    @FXML
+    private TextField field_f;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println(Calculation.get(0,2,"x*x+3*x"));
+        int a = 2; // получаем от пользователя
+        int b = 0; //
+        String f = "x^2+3*x"; //
+
+        // выводим пользователю как ответ
+        System.out.println(Calculation.get(a, b, f));
     }
 
 }
