db334c7392f7197df66e0dc32f240c20414ad569
Eduard <Eduard@EdwardsPersonalLaptop>
2018-05-25 13:34:41 +0300

    - added menubar - edit GUI

--- src/mathdiffer/controller/MainWinController.java
+++ src/mathdiffer/controller/MainWinController.java
@@ -1,21 +1,21 @@
 package mathdiffer.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import mathdiffer.Calculation;
-import mathdiffer.MathDiffer;
 
 public class MainWinController implements Initializable {
 
@@ -27,19 +27,66 @@ public class MainWinController implements Initializable {
 
     @FXML
     private TextField field_form;
-    
+
     @FXML
     private Button btn_enter;
-    
-    @FXML
-    private Button btn_settings;
 
+    @FXML
+    private Label lb_valueEps;
+
+    @FXML
+    private Label lb_valueN;
+
+    @FXML
+    private TextField tx_valueN;
+
+    @FXML
+    private Slider sl_eps;
+
+    @FXML
+    private Slider sl_N;
+
+    BooleanProperty property = new SimpleBooleanProperty(true);
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        sl_eps.valueProperty().addListener((observable, oldValue, newValue) -> {
+            double tempNewValue = Math.round(newValue.doubleValue());
+            sl_eps.setValue(tempNewValue);
+            lb_valueEps.setText(String.valueOf((int) tempNewValue));
+        });
+        sl_N.valueProperty().addListener((observable, oldValue, newValue) -> {
+            double tempNewValue = Math.round(newValue.doubleValue());
+            sl_N.setValue(tempNewValue);
+            lb_valueN.setText(String.valueOf((int) tempNewValue));
+        });
+
+        tx_valueN.visibleProperty().bind(property.not());
+        lb_valueN.visibleProperty().bind(property);
+
+        tx_valueN.managedProperty().bind(property.not());
+        lb_valueN.managedProperty().bind(property);
+
+        tx_valueN.textProperty().bindBidirectional(lb_valueN.textProperty());
+
+        lb_valueN.setOnMouseClicked((event) -> {
+            if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
+                property.set(false);
+            }
+        });
+
+        tx_valueN.setOnKeyPressed((event) -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                property.set(true);
+                sl_N.setValue(Double.valueOf(tx_valueN.getText()));
+            }
+        });
+
+        lb_valueEps.setText(String.valueOf((int) sl_eps.getValue()));
+        lb_valueN.setText(String.valueOf((int) sl_N.getValue()));
+
     }
-    
+
     @FXML
     private void show_result(ActionEvent event) {
         Alert alert = new Alert(AlertType.INFORMATION);
@@ -50,23 +97,13 @@ public class MainWinController implements Initializable {
     }
 
     @FXML
-    private void show_settings(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(MathDiffer.class.getResource("fxml/MainWin.fxml"));
-            Parent root = loader.load();
-            
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            
-            stage.setResizable(false);
-            
-            SettingsWinController controller = loader.getController();
-            // do something 
-            
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException iOException) {
-        }
+    private void show_manual(ActionEvent event) {
+        
     }
+
+    @FXML
+    private void show_about(ActionEvent event) {
     
+    }
+
 }
