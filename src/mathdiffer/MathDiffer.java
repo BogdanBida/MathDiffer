9e9742668984dbf4782f4aabb36f1a57df2e624e
Eduard <Eduard@EdwardsPersonalLaptop>
2018-05-29 03:35:32 +0300

    - create chart - create method for build line

--- src/mathdiffer/MathDiffer.java
+++ src/mathdiffer/MathDiffer.java
@@ -14,8 +14,6 @@ public class MathDiffer extends Application {
         
         Scene scene = new Scene(root);
         
-        stage.setResizable(false);
-        
         stage.setScene(scene);
         stage.show();
     }
--- src/mathdiffer/controller/MainWinController.java
+++ src/mathdiffer/controller/MainWinController.java
@@ -2,8 +2,9 @@ package mathdiffer.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
-import java.util.regex.Pattern;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
@@ -12,54 +13,79 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import mathdiffer.Calculation;
-import mathdiffer.DifferCalc;
 import mathdiffer.MathDiffer;
 
 public class MainWinController implements Initializable {
 
     @FXML
-    private TextField field_a;
+    private Tab int_tab;
 
     @FXML
-    private TextField field_b;
+    private TextField int_field_a;
 
     @FXML
-    private TextField field_form;
+    private TextField int_field_b;
 
     @FXML
-    private Button btn_enter;
+    private TextField int_field_form;
 
     @FXML
-    private Button btn_settings;
+    private Button int_btn_enter;
 
     @FXML
-    private Label lb_valueEps;
+    private Label int_lb_valueEps;
 
     @FXML
-    private Label lb_valueN;
+    private Label int_lb_valueN;
 
     @FXML
-    private TextField tx_valueN;
+    private TextField int_tx_valueN;
 
     @FXML
-    private Slider sl_eps;
+    private Slider int_sl_eps;
 
     @FXML
-    private Slider sl_N;
+    private Slider int_sl_N;
 
-    BooleanProperty property = new SimpleBooleanProperty(true);
+    @FXML
+    private Tab diff_tab;
+
+    @FXML
+    private AnchorPane diff_ach;
+
+    @FXML
+    private TextField diff_form;
+
+    @FXML
+    private TextField diff_x0;
+
+    @FXML
+    private TextField diff_y0;
+
+    @FXML
+    private TextField diff_end;
+
+    NumberAxis diff_chart_xAxis = new NumberAxis();
+    NumberAxis diff_chart_yAxis = new NumberAxis();
+    XYChart<Double, Double> diff_chart = new LineChart(diff_chart_xAxis, diff_chart_yAxis);
+    BooleanProperty int_property = new SimpleBooleanProperty(true);
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -68,70 +94,92 @@ public class MainWinController implements Initializable {
     }
 
     public void initializeIntSolv() {
-        field_form.setOnKeyPressed((event) -> {
+        int_field_form.setOnKeyPressed((event) -> {
             if (event.getCode() == KeyCode.ENTER) {
                 show_result();
             }
         });
 
-        sl_eps.valueProperty().addListener((observable, oldValue, newValue) -> {
+        int_sl_eps.valueProperty().addListener((observable, oldValue, newValue) -> {
             double tempNewValue = Math.round(newValue.doubleValue());
-            sl_eps.setValue(tempNewValue);
-            lb_valueEps.setText(String.valueOf((int) tempNewValue));
+            int_sl_eps.setValue(tempNewValue);
+            int_lb_valueEps.setText(String.valueOf((int) tempNewValue));
         });
-        sl_N.valueProperty().addListener((observable, oldValue, newValue) -> {
+        int_sl_N.valueProperty().addListener((observable, oldValue, newValue) -> {
             double tempNewValue = Math.round(newValue.doubleValue());
-            sl_N.setValue(tempNewValue);
-            lb_valueN.setText(String.valueOf((int) tempNewValue));
+            int_sl_N.setValue(tempNewValue);
+            int_lb_valueN.setText(String.valueOf((int) tempNewValue));
         });
 
-        tx_valueN.visibleProperty().bind(property.not());
-        lb_valueN.visibleProperty().bind(property);
+        int_tx_valueN.visibleProperty().bind(int_property.not());
+        int_lb_valueN.visibleProperty().bind(int_property);
 
-        tx_valueN.managedProperty().bind(property.not());
-        lb_valueN.managedProperty().bind(property);
+        int_tx_valueN.managedProperty().bind(int_property.not());
+        int_lb_valueN.managedProperty().bind(int_property);
 
-        tx_valueN.textProperty().bindBidirectional(lb_valueN.textProperty());
+        int_tx_valueN.textProperty().bindBidirectional(int_lb_valueN.textProperty());
 
-        lb_valueN.setOnMouseClicked((event) -> {
+        int_lb_valueN.setOnMouseClicked((event) -> {
             if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
-                property.set(false);
+                int_property.set(false);
             }
         });
 
-        tx_valueN.setOnKeyPressed((event) -> {
+        int_tx_valueN.setOnKeyPressed((event) -> {
             if (event.getCode() == KeyCode.ENTER) {
-                if (sl_N.getMax() >= Double.valueOf(tx_valueN.getText()) && sl_N.getMin() <= Double.valueOf(tx_valueN.getText())) {
-                    property.set(true);
-                    sl_N.setValue(Double.valueOf(tx_valueN.getText()));
-                    tx_valueN.setStyle("-fx-background-color: white");
+                if (int_sl_N.getMax() >= Double.valueOf(int_tx_valueN.getText()) && int_sl_N.getMin() <= Double.valueOf(int_tx_valueN.getText())) {
+                    int_property.set(true);
+                    int_sl_N.setValue(Double.valueOf(int_tx_valueN.getText()));
+                    int_tx_valueN.setStyle("-fx-background-color: white");
                 } else {
-                    tx_valueN.setStyle("-fx-background-color: red");
+                    int_tx_valueN.setStyle("-fx-background-color: red");
                 }
             }
         });
 
-        lb_valueEps.setText(String.valueOf((int) sl_eps.getValue()));
-        lb_valueN.setText(String.valueOf((int) sl_N.getValue()));
-        field_form.disableProperty().bind(field_a.textProperty().isEmpty().not().and(field_b.textProperty().isEmpty().not()).not());
-        btn_enter.disableProperty().bind(field_form.disableProperty());
-
+        int_lb_valueEps.setText(String.valueOf((int) int_sl_eps.getValue()));
+        int_lb_valueN.setText(String.valueOf((int) int_sl_N.getValue()));
+        int_field_form.disableProperty().bind(int_field_a.textProperty().isEmpty().not().and(int_field_b.textProperty().isEmpty().not()).not());
+        int_btn_enter.disableProperty().bind(int_field_form.disableProperty());
     }
 
     public void initializeDiffer() {
-        double[] test = DifferCalc.get("x*x + 3*y", 0, 5, 1);
-        for (int i = 0; i < 5; i++) {
-            System.out.print(test[i] + " ");
+        diff_ach.getChildren().add(diff_chart);
+        AnchorPane.setTopAnchor(diff_chart, 5.0);
+        AnchorPane.setLeftAnchor(diff_chart, 5.0);
+        AnchorPane.setRightAnchor(diff_chart, 5.0);
+        AnchorPane.setBottomAnchor(diff_chart, 35.0);
+
+        ArrayList<Pair<Double, Double>> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            list.add(new Pair<>(random.nextInt(40) + 10.0, random.nextInt(40) + 10.0));
         }
+        buildLine(list);
+        //        diff_chart.getXAxis().set
+        //        double[] test = DifferCalc.get("x*x + 3*y", 0, 5, 1);
+        //        for (int i = 0; i < 5; i++) {
+        //            System.out.print(test[i] + " ");
+        //        }
+    }
+
+    private void buildLine(ArrayList<Pair<Double, Double>> list) {
+        XYChart.Series<Double, Double> line = new XYChart.Series<>();
+
+        list.forEach((pair) -> {
+            line.getData().add(new XYChart.Data<>(pair.getKey(), pair.getValue()));
+        });
+        line.setName("Test");
+        diff_chart.getData().add(line);
     }
 
     @FXML
     public void show_result() {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Ответ");
-        alert.setHeaderText(field_form.getText());
-        String a = field_a.getText();
-        String b = field_b.getText();
+        alert.setHeaderText(int_field_form.getText());
+        String a = int_field_a.getText();
+        String b = int_field_b.getText();
 
         try {
             a = String.valueOf(Calculation.f(0, a));
@@ -142,7 +190,7 @@ public class MainWinController implements Initializable {
             return;
         }
 
-        alert.setContentText(Calculation.get(Double.valueOf(a), Double.valueOf(b), field_form.getText()));
+        alert.setContentText(Calculation.get(Double.valueOf(a), Double.valueOf(b), int_field_form.getText()));
         alert.show();
     }
 
